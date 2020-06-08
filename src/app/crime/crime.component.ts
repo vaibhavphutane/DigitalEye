be0b9e9238222c01eade0eb6bd90852899f9227c
Vaibhav Phutane <vaibhav.phutane@nihilent.com>
2020-06-08 14:49:43 +0200

    Final changes

--- src/app/crime/crime.component.ts
+++ src/app/crime/crime.component.ts
@@ -5,24 +5,22 @@ import { CrimeService } from '../crime.service';
   selector: 'app-crime',
   templateUrl: './crime.component.html',
   styleUrls: ['./crime.component.css'],
-  providers: []
+  providers: [],
 })
 export class CrimeComponent implements OnInit {
-
-
   images: any[];
   selectedId: number;
   processedImageURL: string;
   isProccessing: boolean;
   objects: string;
 
-  constructor(private crimeService: CrimeService) { }
+  constructor(private crimeService: CrimeService) {}
 
   ngOnInit(): void {
-    this.images = this.crimeService.files.map((item, index) => {
+    this.images = this.crimeService.files && this.crimeService.files.map((item, index) => {
       return {
         id: index,
-        image: item
+        image: item,
       };
     });
     console.log(this.images);
@@ -31,11 +29,13 @@ export class CrimeComponent implements OnInit {
   processImage(img) {
     const crimeSceneImage = new File([img], 'crimeImage.png');
     this.isProccessing = true;
-    this.crimeService.postImageAPI(crimeSceneImage).subscribe(res => {
+    this.crimeService.postImageAPI(crimeSceneImage).subscribe((res) => {
       this.isProccessing = false;
       this.processedImageURL = '../../assets/images/img10.PNG';
       this.objects = 'Pills / Syringe';
     });
   }
-}
 
+
+
+}
